d471281541fa7151c01ce18abc9fe3821d279f59
Felipe <felipejmesina@gmail.com>
2017-08-23 19:08:08 -0700

    completed jquery functions assignment

---
+++ jQueryFunctions.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>JavaScript</title>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script>
+            $(document).ready(function(){
+                $("#top_button").click(function(){
+                    $("boo").show();
+                });
+            
+
+
+
+            });
+        </script>
+    </head>
+    <body>
+        <button id="top_button">Do you are?...</button>
+        <p id="boo">BOO!</p>
+
+
+
+
+    
+    </body>
+</html>
